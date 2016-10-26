fe804b8293a3190a79105d20e43c8e2e80c9de48
gillchristian <gillchristiang@gmail.com>
2016-10-25 23:50:07 -0300

    Add GitKraken download link to Setup.

--- presentation/08.Setup.js
+++ presentation/08.Setup.js
@@ -9,9 +9,14 @@ export default function Setup() {
   return (
     <div>
       <Heading caps textColor="primary">Setup</Heading>
-      <Heading size={4}>
+      <Heading size={4} margin="50px auto 25px">
         <Link href="https://git-scm.com/download" target="_blank" textColor="quaternary">
           git-scm.com/download
+        </Link>
+      </Heading>
+      <Heading size={4}>
+        <Link href="https://www.gitkraken.com/download" target="_blank" textColor="quaternary">
+          gitkraken.com/download
         </Link>
       </Heading>
     </div>
